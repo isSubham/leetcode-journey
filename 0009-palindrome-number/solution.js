7bd9f51d7c10c9def2ce307a57b0e11138bcb099
Subham Haldar <82716446+isSubham@users.noreply.github.com>
2026-04-12 14:36:16 +0000

    Sync LeetCode submission Runtime - 8 ms (46.55%), Memory - 63.9 MB (57.99%)

--- 0009-palindrome-number/solution.js
+++ 0009-palindrome-number/solution.js
@@ -3,25 +3,19 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    // const INT_MAX = 2 ** 31 - 1; 
-    // const MAX_DIV_10 = Math.trunc(INT_MAX / 10);
 
-    if(x<0) return false
-    if(x % 10 === 0 && x!==0) return false
+    // if (x === 0) return true
+    if (x < 0) return false
 
+    let org = x
     let rev = 0;
-    let original = x
 
-    while (x !== 0) {
+    while (x > 0) {
         let lastDigit = x % 10;
-        x = Math.trunc(x / 10);
-
-        // if (rev > MAX_DIV_10 || (rev === MAX_DIV_10 && lastDigit > 7)) return false;
-
         rev = rev * 10 + lastDigit;
+        x = (x / 10) | 0;
     }
 
-    if(original === rev) return true
-    else return false
 
+    return org === rev;
 };
